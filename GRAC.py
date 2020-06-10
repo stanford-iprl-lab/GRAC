7fdf7fd82d1ee4f16272602b05bff923a45c8070
Ubuntu <ubuntu@ip-172-31-24-132.us-west-2.compute.internal>
2020-06-10 00:00:11 +0000

    add GRAC

--- GRAC.py
+++ GRAC.py
@@ -130,11 +130,11 @@ class GRAC(GRAC_base):
 	
 		ACTOR_LR_START = {
 			'Ant-v2': 3e-4,
-			'Humanoid-v2': 2e-4,
-                        'HalfCheetah-v2': 3e-4,
-                        'Hopper-v2': 3e-4,
-                        'Swimmer-v2': 3e-4,
-                        'Walker2d-v2': 3e-4,
+			'Humanoid-v2': 3e-4,
+            'HalfCheetah-v2': 3e-4,
+            'Hopper-v2': 3e-4,
+            'Swimmer-v2': 3e-4,
+            'Walker2d-v2': 3e-4,
 		}
 		self.actor_lr_start = ACTOR_LR_START[env]
 
@@ -193,7 +193,7 @@ class GRAC(GRAC_base):
                         'Ant-v2': 1./float(self.action_dim),
                         'Humanoid-v2': 1./float(self.action_dim),
                         'HalfCheetah-v2': 1./float(self.action_dim),
-                        'Hopper-v2': 0.3/float(self.action_dim),
+                        'Hopper-v2': 1.0/float(self.action_dim),
                         'Swimmer-v2': 1./float(self.action_dim),
                         'Walker2d-v2': 1.0/float(self.action_dim),
 		}
